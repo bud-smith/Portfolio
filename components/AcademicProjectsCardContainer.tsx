42397e8c81ff636dd2a90ca51d11db063f0361db
Bud Smith <35976655+bud-smith@users.noreply.github.com>
2020-11-02 04:27:45 +1000

    Fixed svg view on modal.

--- components/AcademicProjectsCardContainer.tsx
+++ components/AcademicProjectsCardContainer.tsx
@@ -24,7 +24,7 @@ export default class AcademicProjectsCardContainer extends Component {
                     category: "Artificial Intelligence",
                     description:
                         "N-Queens is the problem of placing n number of queens on a chessboard of size n×n in which no queens can attack each other. This implementation uses an uninformed search strategy to find all solutions being breadth-first search (BFS), and two local search strategies to find a single solution being hill-climbing search and simulated annealing (SA) search.",
-                    technologies: ["C++"],
+                    technologies: ["c++"],
                     demo: "https://n-queens.buds1.repl.run/",
                     source:
                         "https://github.com/bud-smith/University-Projects/tree/main/N-Queens",
@@ -37,11 +37,11 @@ export default class AcademicProjectsCardContainer extends Component {
                     description:
                         "Alarm Surgo is a hybrid mobile application. The apps features include creating, editing, and deleting alarms, checking the latest news and weather using various public API's, and choosing a personalised location to view the news and weather from anywhere in the world.",
                     technologies: [
-                        "Angular",
-                        "Ionic",
-                        "TypeScript",
-                        "HTML5",
-                        "CSS",
+                        "angular",
+                        "ionic",
+                        "typescript",
+                        "html5",
+                        "css3",
                     ],
                     demo:
                         "https://appetize.io/app/dadcbhk6xkj3p3gf5cv35xxjfc?device=pixel4xl&scale=75&orientation=portrait&osVersion=10.0&deviceColor=black",
--- components/ModalView.tsx
+++ components/ModalView.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 
-function MyVerticallyCenteredModal(props) {
+function VerticallyCenteredModal(props) {
     return (
         <Modal
             {...props}
@@ -18,10 +18,24 @@ function MyVerticallyCenteredModal(props) {
             <Modal.Body>
                 <h4>{props.projects_list_prop.name}</h4>
                 <p>{props.projects_list_prop.description}</p>
+                <ul>
+                    {props.projects_list_prop.technologies.map((item) => (
+                        <img
+                            className="svg-size"
+                            src={"../images/technologies/" + item + ".svg"}
+                        />
+                    ))}
+                </ul>
             </Modal.Body>
             <Modal.Footer>
                 <Button onClick={props.onHide}>Close</Button>
             </Modal.Footer>
+            <style jsx>{`
+                .svg-size {
+                    max-width: 48px;
+                    max-height: 48px;
+                }
+            `}</style>
         </Modal>
     );
 }
@@ -33,7 +47,7 @@ const ModalView = (props) => {
         <>
             <div onClick={() => setModalShow(true)}>{props.children}</div>
 
-            <MyVerticallyCenteredModal
+            <VerticallyCenteredModal
                 projects_list_prop={props.projects_list_prop}
                 show={modalShow}
                 onHide={() => setModalShow(false)}
